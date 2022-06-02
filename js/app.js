dd9f9814e224c6b9aca6d0e5b0749bffaa3f790e
Oscar Farfan <oscarfarfan92@gmail.com>
2022-06-02 16:47:20 -0500

    Se crean funcionalidades de los botones y se agrega información dinámica a la tabla de ranking

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 import { data } from "./data.js";
 
+let ranking = [];
+let user = '';
+let numberQuestion = 0;
+let level = 1;
+let score = 0;
+let scoreRound = new Map();
+scoreRound.set(1, 100);
+scoreRound.set(2, 300);
+scoreRound.set(3, 500);
+scoreRound.set(4, 800);
+scoreRound.set(5, 1000);
+let jugador;
+
 loadHomePage();
 
 function blankPage() {
@@ -62,27 +75,27 @@ function loadThanksPage() {
     titleThankYou.textContent = '¡Gracias por Participar!';
     thankYouContainer.appendChild(titleThankYou);
 
-    const score = document.createElement('div');
-    score.className = 'score';
+    const scoreParcial = document.createElement('div');
+    scoreParcial.className = 'score';
     const titleScore = document.createElement('span');
     titleScore.textContent = 'Puntos Ganados:';
     const inputScore = document.createElement('input');
     inputScore.type = 'text';
     inputScore.id = 'score';
     inputScore.name = 'score';
-    inputScore.value = '1450';
+    inputScore.value = score;
     inputScore.disabled = true;
     const br = document.createElement('br');
     const buttonReturnIndex = document.createElement('button');
     buttonReturnIndex.className = 'btn-score';
     buttonReturnIndex.textContent = 'Volver al Inicio';
-    score.appendChild(titleScore);
-    score.appendChild(inputScore);
-    score.appendChild(br);
-    score.appendChild(buttonReturnIndex);
+    scoreParcial.appendChild(titleScore);
+    scoreParcial.appendChild(inputScore);
+    scoreParcial.appendChild(br);
+    scoreParcial.appendChild(buttonReturnIndex);
 
     containerThanks.appendChild(thankYouContainer);
-    containerThanks.appendChild(score);
+    containerThanks.appendChild(scoreParcial);
 
     body.appendChild(containerThanks);
     goToTheHomePage();
@@ -105,14 +118,30 @@ function loadPlayersPage() {
     const puntajeLogrado = document.createElement('th');
     puntajeLogrado.textContent = 'Puntaje Logrado';
 
+    tableHeader.appendChild(nombreUsuario);
+    tableHeader.appendChild(puntajeLogrado);
+    tablePlayers.appendChild(tableHeader);
+
+    ranking.forEach(dato => { 
+    const tr = document.createElement("tr");
+    const tdJugador = document.createElement("td");
+    const tdPuntaje = document.createElement("td");
+
+    tdJugador.textContent = dato.usuario;
+    tdPuntaje.textContent = dato.puntaje;
+
+    tr.appendChild(tdJugador);
+    tr.appendChild(tdPuntaje);
+
+    tablePlayers.appendChild(tr);
+    
+    });
+
     const buttonReturnIndex = document.createElement('button');
     buttonReturnIndex.className = 'btn-score';
     buttonReturnIndex.textContent = 'Volver al Inicio';
 
     containerPlayers.appendChild(buttonReturnIndex);
-    tableHeader.appendChild(nombreUsuario);
-    tableHeader.appendChild(puntajeLogrado);
-    tablePlayers.appendChild(tableHeader);
     containerPlayers.appendChild(tablePlayers);
 
     body.appendChild(containerPlayers);
@@ -141,25 +170,29 @@ function loadGamePage() {
     lbNombreUsuario.textContent = 'Nombre de Usuario';
     const inputNombreUsuario = document.createElement('input');
     inputNombreUsuario.type = 'text';
-    inputNombreUsuario.readOnly = true;
+    inputNombreUsuario.disabled = true;
+    inputNombreUsuario.value = user;
 
     const lbNivel = document.createElement('label');
     lbNivel.textContent = 'Nivel';
     const inputNivel = document.createElement('input');
     inputNivel.type = 'text';
-    inputNivel.readOnly = true;
+    inputNivel.disabled = true;
+    inputNivel.value = level;
 
     const lbPuntosObtenidos = document.createElement('label');
     lbPuntosObtenidos.textContent = 'Puntos Obtenidos';
     const inputPuntosObtenidos = document.createElement('input');
     inputPuntosObtenidos.type = 'text';
-    inputPuntosObtenidos.readOnly = true;
+    inputPuntosObtenidos.disabled = true;
+    inputPuntosObtenidos.value = score;
 
     const lbPuntosRonda = document.createElement('label');
     lbPuntosRonda.textContent = 'Puntos de la Ronda';
     const inputPuntosRonda = document.createElement('input');
     inputPuntosRonda.type = 'text';
-    inputPuntosRonda.readOnly = true;
+    inputPuntosRonda.disabled = true;
+    inputPuntosRonda.value = scoreRound.get(level);
 
     containerInfoGame.appendChild(lbNombreUsuario);
     containerInfoGame.appendChild(inputNombreUsuario);
@@ -168,6 +201,7 @@ function loadGamePage() {
     containerInfoGame.appendChild(inputNivel);
     containerInfoGame.appendChild(br2);
     containerInfoGame.appendChild(lbPuntosObtenidos);
+    containerInfoGame.appendChild(inputPuntosObtenidos);
     containerInfoGame.appendChild(br3);
     containerInfoGame.appendChild(lbPuntosRonda);
     containerInfoGame.appendChild(inputPuntosRonda);
@@ -176,57 +210,68 @@ function loadGamePage() {
     containerCuestions.className = 'container-questions';
 
     const formCuestions = document.createElement('form');
-    
+    formCuestions.className = 'form-questions';
+
+    if (level === 1) {
+        numberQuestion = Math.floor((Math.random() * (4 - 0 + 1)) + 0);
+    } else if (level === 2) {
+        numberQuestion = Math.floor((Math.random() * (9 - 5 + 1)) + 5);
+    } else if (level === 3) {
+        numberQuestion = Math.floor((Math.random() * (14 - 10 + 1)) + 10);
+    } else if (level === 4) {
+        numberQuestion = Math.floor((Math.random() * (19 - 15 + 1)) + 15);
+    } else if (level === 5) {
+        numberQuestion = Math.floor((Math.random() * (24 - 20 + 1)) + 20);
+    }
+
+    console.log(numberQuestion);
+
     const cuestion = document.createElement('p');
-    cuestion.textContent = '¿Cuál es la capital de Bogotá?';
+    cuestion.textContent = data[numberQuestion].pregunta;
 
     const inputOption1 = document.createElement('input');
     inputOption1.type = 'radio';
-    inputOption1.id = 'bogota';
-    inputOption1.name = 'nivel1';
-    inputOption1.value = 'Bogotá';
+    inputOption1.id = 'opcionA';
+    inputOption1.name = 'opciones';
+    inputOption1.value = data[numberQuestion].opciones.a;
     const lbOption1 = document.createElement('label');
-    lbOption1.for = 'bogota';
-    lbOption1.textContent = 'Bogotá';
+    lbOption1.for = 'opcionA';
+    lbOption1.textContent = data[numberQuestion].opciones.a;
 
     const inputOption2 = document.createElement('input');
     inputOption2.type = 'radio';
-    inputOption2.id = 'cali';
-    inputOption2.name = 'nivel1';
-    inputOption2.value = 'Cali';
+    inputOption2.id = 'opcionB';
+    inputOption2.name = 'opciones';
+    inputOption2.value = data[numberQuestion].opciones.b;
     const lbOption2 = document.createElement('label');
-    lbOption2.for = 'cali';
-    lbOption2.textContent = 'Cali';
+    lbOption2.for = 'opcionB';
+    lbOption2.textContent = data[numberQuestion].opciones.b;
 
     const inputOption3 = document.createElement('input');
     inputOption3.type = 'radio';
-    inputOption3.id = 'medellin';
-    inputOption3.name = 'nivel1';
-    inputOption3.value = 'Medellín';
+    inputOption3.id = 'opcionC';
+    inputOption3.name = 'opciones';
+    inputOption3.value = data[numberQuestion].opciones.c;
     const lbOption3 = document.createElement('label');
-    lbOption3.for = 'medellin';
-    lbOption3.textContent = 'Medellín';
+    lbOption3.for = 'opcionC';
+    lbOption3.textContent = data[numberQuestion].opciones.c;
 
     const inputOption4 = document.createElement('input');
     inputOption4.type = 'radio';
-    inputOption4.id = 'barranquilla';
-    inputOption4.name = 'nivel1';
-    inputOption4.value = 'Barranquilla';
+    inputOption4.id = 'opcionD';
+    inputOption4.name = 'opciones';
+    inputOption4.value = data[numberQuestion].opciones.d;
     const lbOption4 = document.createElement('label');
-    lbOption4.for = 'barranquilla';
-    lbOption4.textContent = 'Barranquilla';
+    lbOption4.for = 'opcionD';
+    lbOption4.textContent = data[numberQuestion].opciones.d;
 
-    const btnResponder = document.createElement('input');
-    btnResponder.type = 'submit';
-    btnResponder.name = 'btn-responder';
-    btnResponder.id = 'responder';
-    btnResponder.value = 'Responder';
+    const btnResponder = document.createElement('button');
+    btnResponder.className = 'btn-responder';
+    btnResponder.textContent = 'Responder';
 
-    const btnRetirarse = document.createElement('input');
-    btnRetirarse.type = 'submit';
-    btnRetirarse.name = 'btn-retirarse';
-    btnRetirarse.id = 'retirarse';
-    btnRetirarse.value = 'Retirarse';
+    const btnRetirarse = document.createElement('button');
+    btnRetirarse.className = 'btn-retirarse';
+    btnRetirarse.textContent = 'Retirarse';
 
     formCuestions.appendChild(cuestion);
     formCuestions.appendChild(inputOption1);
@@ -251,19 +296,93 @@ function loadGamePage() {
     containerGame.appendChild(containerCuestions);
 
     body.appendChild(containerGame);
+    responder();
+    retirarse();
+}
+
+function validarRespuesta() {
+    let verifica = false;
+    if (document.getElementById('opcionA').checked && document.getElementById('opcionA').value == data[numberQuestion].respuesta) {
+        verifica = true;
+    }else if (document.getElementById('opcionB').checked && document.getElementById('opcionB').value == data[numberQuestion].respuesta) {
+        verifica = true;
+    }else if (document.getElementById('opcionC').checked && document.getElementById('opcionC').value == data[numberQuestion].respuesta) {
+        verifica = true;
+    }else if (document.getElementById('opcionD').checked && document.getElementById('opcionD').value == data[numberQuestion].respuesta) {
+        verifica = true;
+    } 
+    return verifica;
+}
+
+function responder() {
+    const botonResponder = document.querySelector('.btn-responder');
+    botonResponder.addEventListener('click', () => {
+        if (validarRespuesta()) {
+            alert("Respuesta correcta");
+            score += scoreRound.get(level);
+            if (level !== 5) {
+                level++;
+                blankPage();
+                loadGamePage();
+            } else {
+                blankPage();
+                loadThanksPage();
+                alert("¡Felicidades, has respondido todas las preguntas correctamente!");
+                jugador = {
+                    usuario: user,
+                    puntaje: score
+                };
+                ranking.push(jugador);
+                score = 0;
+                level = 1;
+            }
+        } else {
+            score = 0;
+            alert("Respuesta incorrecta, has perdido todos tus puntos lastimosamente");
+            level = 1;
+            jugador = {
+                usuario: user,
+                puntaje: score
+            };
+            ranking.push(jugador);
+            blankPage();
+            loadThanksPage();
+        }
+    });
+}
+
+function retirarse() {
+    const botonRetirarse = document.querySelector('.btn-retirarse');
+    botonRetirarse.addEventListener('click', () => {
+        jugador = {
+            usuario: user,
+            puntaje: score
+        };
+        ranking.push(jugador);
+        blankPage();
+        loadThanksPage();
+        level = 1;
+        alert('¡Has tomado la decisión de retirarte, si ganaste puntos no los perderás!');
+    });
 }
 
 function goToTheGamePage() {
     const gameButton = document.querySelector('.btn-game');
     gameButton.addEventListener('click', () => {
-        blankPage();
-        loadGamePage();
+        user = document.getElementById('username').value;
+        if (user == '') {
+            alert('Por favor ingresa un nombre de usuario');
+        } else {
+            blankPage();
+            loadGamePage();
+        }
     });
 }
 
 function goToTheHomePage() {
     const homeButton = document.querySelector('.btn-score');
     homeButton.addEventListener('click', () => {
+        score = 0;
         blankPage();
         loadHomePage();
     });
@@ -277,10 +396,3 @@ function goToThePlayersPage() {
     });
 }
 
-// function goToTheThanksPage() {
-//     const playersButton = document.querySelector('.btn-view-table-players');
-//     playersButton.addEventListener('click', () => {
-//         blankPage();
-//         loadPlayersPage();
-//     });
-// }
